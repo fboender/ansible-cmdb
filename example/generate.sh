d7bd63cafc1783b0c15b831586982ac7d80ef73c
Ferry Boender <ferry.boender@gmail.com>
2015-08-23 11:29:51 +0200

    Rewrite of inventory parsing to support recursive children groups.
    Still needs some cleanups.

--- example/generate.sh
+++ example/generate.sh
@@ -2,6 +2,10 @@
 
 export PYTHONPATH=../lib
 
+##
+## Default example
+##
+
 # Python v2
 python2 ../src/ansible-cmdb -i hosts out > html_fancy.html
 python2 ../src/ansible-cmdb -t txt_table -i hosts out > txt_table.txt
@@ -9,3 +13,15 @@ python2 ../src/ansible-cmdb -t txt_table -i hosts out > txt_table.txt
 # Python v3
 python3 ../src/ansible-cmdb -i hosts out > html_fancy.html
 python3 ../src/ansible-cmdb -t txt_table -i hosts out > txt_table.txt
+
+##
+## Complicated hosts file
+##
+
+# Python v2
+python2 ../src/ansible-cmdb -i hosts_complicated out > html_fancy_complicated.html
+python2 ../src/ansible-cmdb -t txt_table -i hosts_complicated out > txt_table_complicated.txt
+
+# Python v3
+python3 ../src/ansible-cmdb -i hosts_complicated out > html_fancy_complicated.html
+python3 ../src/ansible-cmdb -t txt_table -i hosts_complicated out > txt_table_complicated.txt
