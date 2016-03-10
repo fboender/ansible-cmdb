3abc48d870b0be010278f899972788120ada5a25
Ferry Boender <ferry.boender@gmail.com>
2016-03-10 07:54:07 +0100

    Properly name output of generate.sh

--- example/generate.sh
+++ example/generate.sh
@@ -17,8 +17,8 @@ echo "python2 ../src/ansible-cmdb -t txt_table -i hosts out > gen_txt_table_2.tx
 python2 ../src/ansible-cmdb -t txt_table -i hosts out > gen_txt_table_2.txt
 echo "python2 ../src/ansible-cmdb -t csv -i hosts out > gen_csv_2.csv"
 python2 ../src/ansible-cmdb -t csv -i hosts out > gen_csv_2.csv
-echo "python2 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_3.csv"
-python2 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_3.csv
+echo "python2 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_2.md"
+python2 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_2.md
 
 # Python v3
 echo "python3 ../src/ansible-cmdb -i hosts out > gen_html_fancy_3.html"
@@ -29,5 +29,5 @@ echo "python3 ../src/ansible-cmdb -t txt_table -i hosts out > gen_txt_table_3.tx
 python3 ../src/ansible-cmdb -t txt_table -i hosts out > gen_txt_table_3.txt
 echo "python3 ../src/ansible-cmdb -t csv -i hosts out > gen_csv_3.csv"
 python3 ../src/ansible-cmdb -t csv -i hosts out > gen_csv_3.csv
-echo "python3 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_3.csv"
-python3 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_3.csv
+echo "python3 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_3.md"
+python3 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_3.md
