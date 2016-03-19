8db1803296cc615eba4478a06265966cf5521906
Ferry Boender <ferry.boender@gmail.com>
2016-03-19 08:22:24 +0100

    Support for sets in util.deepupdate. Fixes #66

--- src/ansiblecmdb/util.py
+++ src/ansiblecmdb/util.py
@@ -4,7 +4,8 @@ def deepupdate(target, src):
     """Deep update target dict with src
     For each k,v in src: if k doesn't exist in target, it is deep copied from
     src to target. Otherwise, if v is a list, target[k] is extended with
-    src[k]. If v is a dict, recursively deep-update it.
+    src[k]. If v is a set, target[k] is updated with v, If v is a dict,
+    recursively deep-update it.
 
     Examples:
     >>> t = {'name': 'Ferry', 'hobbies': ['programming', 'sci-fi']}
@@ -23,6 +24,11 @@ def deepupdate(target, src):
                 target[k] = copy.deepcopy(v)
             else:
                 deepupdate(target[k], v)
+        elif type(v) == set:
+            if not k in target:
+                target[k] = v.copy()
+            else:
+                target[k].update(v.copy())
         else:
             target[k] = copy.copy(v)
 
