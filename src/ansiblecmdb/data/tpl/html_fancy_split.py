d4defd9215c83cab77678a6c3a0558485f8b639b
Ferry Boender <ferry.boender@electricmonk.nl>
2017-06-13 18:37:06 +0200

    Use codecs for utf8 output for python2/3 compatibility

--- src/ansiblecmdb/data/tpl/html_fancy_split.py
+++ src/ansiblecmdb/data/tpl/html_fancy_split.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import codecs
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
@@ -17,12 +18,15 @@ def render(hosts, vars={}, tpl_dirs=[]):
 
     # Render host overview
     template = lookup.get_template('html_fancy_split_overview.tpl')
-    with open('cmdb/index.html', 'w') as f:
-        f.write(template.render(hosts=hosts, **vars).lstrip())
+    out_file = os.path.join('cmdb', 'index.html')
+    output = template.render(hosts=hosts, **vars).lstrip().decode('utf8')
+    with codecs.open(out_file, 'w', encoding='utf8') as f:
+        f.write(output)
 
     # Render host details
     template = lookup.get_template('html_fancy_split_detail.tpl')
     for hostname, host in hosts.items():
         out_file = os.path.join('cmdb', '{0}.html'.format(hostname))
-        with open(out_file, 'w') as f:
-            f.write(template.render(host=host, **vars).lstrip())
+        output = template.render(host=host, **vars).lstrip().decode('utf8')
+        with codecs.open(out_file, 'w', encoding='utf8') as f:
+            f.write(output)
