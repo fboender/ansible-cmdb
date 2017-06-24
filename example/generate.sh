938055f5b1b9693ef2bc9d4f99bd0f79049956bb
Ferry Boender <ferry.boender@electricmonk.nl>
2017-06-24 09:13:48 +0200

    Generate example fact_cache versions

--- example/generate.sh
+++ example/generate.sh
@@ -17,6 +17,8 @@ python2 ../src/ansible-cmdb -t csv -i hosts out > gen_csv_2.csv
 python2 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_2.md
 python2 ../src/ansible-cmdb -t sql -i hosts out > gen_sql_2.md
 python2 ../src/ansible-cmdb -t html_fancy_split -i hosts out > gen_html_fancy_split_2.md
+python2 ../src/ansible-cmdb -i hosts -f out_factcache > gen_fact_cache_2.html
+
 
 # Python v3
 python3 ../src/ansible-cmdb -i hosts out > gen_html_fancy_3.html
@@ -26,3 +28,4 @@ python3 ../src/ansible-cmdb -t csv -i hosts out > gen_csv_3.csv
 python3 ../src/ansible-cmdb -t markdown -i hosts out > gen_markdown_3.md
 python3 ../src/ansible-cmdb -t sql -i hosts out > gen_sql_3.md
 python3 ../src/ansible-cmdb -t html_fancy_split -i hosts out
+python3 ../src/ansible-cmdb -i hosts -f out_factcache > gen_fact_cache_3.html
